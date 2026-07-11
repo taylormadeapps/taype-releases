b81c826f5fac0a7c788e3fbe7d145bc94b07e50e
Tom Taylor <ttaylor@encircle.co.uk>
2026-07-11 22:56:36 +0100

    Release v1.1.0

--- assets/site.js
+++ assets/site.js
@@ -1,6 +1,6 @@
 const PUBLIC_SITE_URL = "https://taype.ruminantaudioworks.com";
 const DOCS_BASE_URL = "https://ruminant-audio-works.gitbook.io/taype/";
-const DOWNLOAD_URL = "https://github.com/taylormadeapps/taype-releases/releases/download/v1.0.4/Taype-1.0.4.pkg";
+const DOWNLOAD_URL = "https://github.com/taylormadeapps/taype-releases/releases/download/v1.1.0/Taype-1.1.0.pkg";
 const BETA_PROGRAMME_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdcIUQBf7p4VecwyEkBmjmVXXXwR09o0xaJLR3r03aqI7KZVg/viewform";
 const PLATFORM_INTEREST_URL = "/other-platforms/";
 const RAW_ABOUT_URL = "/ruminant-audio-works/";
